b2fdddc112d145d4c408fded166705a385738468
Sumit Rai <developer2607@gmail.com>
2022-01-19 14:23:12 +0530

    Project Structure Created

--- app/src/main/java/com/example/squarboatassignment/ui/MainActivity.kt
+++ app/src/main/java/com/example/squarboatassignment/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.squarboatassignment
+package com.example.squarboatassignment.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.squarboatassignment.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
