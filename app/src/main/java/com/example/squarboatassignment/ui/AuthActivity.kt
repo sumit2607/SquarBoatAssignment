6da854d9cff9fdf54e1bc8f1b9677ab5c48cd5f0
Sumit Rai <developer2607@gmail.com>
2022-01-19 14:30:11 +0530

    Navigation and ui related

---
+++ app/src/main/java/com/example/squarboatassignment/ui/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.squarboatassignment.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.squarboatassignment.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
