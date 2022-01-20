5b4b68cb25c50a119fa7e0ee5545e4d855568402
Sumit Rai <developer2607@gmail.com>
2022-01-21 00:15:33 +0530

    Splash Screen done

--- app/src/main/java/com/example/squarboatassignment/ui/ChoseOne.kt
+++ app/src/main/java/com/example/squarboatassignment/ui/ChoseOne.kt
@@ -4,12 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
 import com.example.squarboatassignment.R
+import com.example.squarboatassignment.ui.CandidateUi.LoginActivity
+import com.example.squarboatassignment.ui.RecruiterUi.RecruiterLoginActivity
+import kotlinx.android.synthetic.main.activity_chose_one.*
 
 class ChoseOne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chose_one)
 
+
+        buTestUpdateText2.setOnClickListener{
+            val intent = Intent(this , RecruiterLoginActivity::class.java)
+            startActivity(intent)
+        }
+        button.setOnClickListener{
+            val intent = Intent(this , LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
+
+
