203bf97c242bb53de846279541917f032849fcd9
Sumit Rai <developer2607@gmail.com>
2022-01-20 23:21:21 +0530

    Login SignUp and ForgetPassword Ui Created

---
+++ app/src/main/java/com/example/squarboatassignment/ui/CandidateUi/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.squarboatassignment.ui.CandidateUi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.squarboatassignment.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
